81c334de83a01fcc7e882e079094055385d37d08
Pappskiva <alleo92@hotmail.com>
2015-08-05 22:14:22 +0200

    pre obj

--- Template_Win8SDK_VS2012/Code/Object.cpp
+++ Template_Win8SDK_VS2012/Code/Object.cpp
@@ -1,11 +1,23 @@
 #include "Object.h"
 
 
-Object::Object()
+Object::Object(){}
+Object::~Object(){}
+void Object::Initialize(std::string p_objName)
 {
+	m_objectName = p_objName;
+}
+void Object::Update()
+{
+
+}
+void Object::Shutdown()
+{
+
 }
 
-
-Object::~Object()
+void Object::LoadObj()
 {
+
 }
+
--- Template_Win8SDK_VS2012/Code/Object.h
+++ Template_Win8SDK_VS2012/Code/Object.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <string>
 class Object
 {
 public:
 	Object();
 	~Object();
+	void Initialize(std::string p_objName);
+	void Update();
+	void Shutdown();
+
+private:
+	void LoadObj();
+	std::string m_objectName;
 };
 
